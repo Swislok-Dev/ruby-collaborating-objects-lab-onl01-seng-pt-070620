73db2622136ad1cfe4def432f43187d6638f9627
Cory Marker <cmarker4@gmail.com>
2020-08-19 14:55:00 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,4 +9,11 @@ class MP3Importer
     @files = Dir.glob("#{@path} /*.mp3").collect{ |file|
     file.gsub("#{@path}/", "") }
   end
-  
+
+  def import
+    self.flies.each{|file|
+    Song.new_by_filename(file)}
+  end
+
+end
+
